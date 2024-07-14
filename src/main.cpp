ce92fafee5394e280f67802667b2ca5a07be8dd6
alphadev <39606824+Alphyyyy@users.noreply.github.com>
2024-07-14 13:01:52 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,32 @@ class $modify(MyMenuLayer, MenuLayer) {
 		if (!fs::exists(img)) return true;
 		profileImage->removeFromParent();
 		if (Mod::get()->getSettingValue<bool>("circle") == false) { 
-			auto newButton = CCSprite::create(img.string().c_str());
+			CCSprite* newButton = CCSprite::create(img.string().c_str());
 			//profileButton->addChild(newButton);
 			newButton->setPosition({profileImage->getPositionX(),profileImage->getPositionY()});
-			newButton->setScale(48 / newButton->getContentSize().height);
+			newButton->setScale(48 / newButton->getContentSize().width);
 			if (Mod::get()->getSettingValue<bool>("circleb") == true) {
 				auto clippingNode = CCClippingNode::create();
-				auto mask = CCDrawNode::create();
-				mask->drawCircle(newButton->getPosition(),24,ccc4FFromccc3B({ 255, 255, 255 }),0.f,ccc4FFromccc3B({ 255, 255, 255 }),23);
-				clippingNode->setStencil(mask);
-				clippingNode->setPosition(mask->getPosition());
+				clippingNode->setContentSize(newButton->getContentSize());
+				clippingNode->setAnchorPoint({0.5f,0.5f});
+				clippingNode->ignoreAnchorPointForPosition(true);
+				clippingNode->setZOrder(0);
+				clippingNode->setInverted(false);
+				clippingNode->setAlphaThreshold(0.7f);
+				auto mask = CCSprite::createWithSpriteFrameName("d_circle_02_001.png");
+				mask->setAnchorPoint({0.5f,0.5f});
+				mask->setColor({255,255,255});
+				mask->setPosition(newButton->getPosition());
+				mask->setScale(.9f);
+				//mask->setScale(0.8f);
+				mask->setZOrder(1);
 				clippingNode->addChild(newButton);
+				clippingNode->setStencil(mask);
+				//clippingNode->setPosition(mask->getPosition());
 				profileButton->addChild(clippingNode);
 				CCSprite* border = CCSprite::create("borderProfile.png"_spr);
 				border->setPosition(newButton->getPosition());
-				border->setZOrder(1);
+				border->setZOrder(2);
 				profileButton->addChild(border);
 			}else{
 				profileButton->addChild(newButton);
